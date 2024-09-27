6cb7184c26be68746647135b920a5b074799905d
MohammadNasimi <mohammadnasimi6803@gmail.com>
2024-09-27 13:17:25 +0330

    add last method test with subTest

--- more_itertools_/test_more.py
+++ more_itertools_/test_more.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from more import take, chunked_ ,first
+from more import take, chunked_ ,first, last
 import traceback
 
 class TakeTest(TestCase):
@@ -75,3 +75,34 @@ class FirstTest(TestCase):
             self.assertIn("first() was called on an empty iterable", formatted_exe)
         else:
             self.fail()                    
+            
+        
+class LastTests(TestCase):
+    def test_basic(self):
+        cases = [
+            (range(4),3),
+            (iter(range(4)),3),
+            (range(1),0),
+            (iter(range(1)),0),
+            ({n:str(n) for n in range(5)},4)
+        ]
+        for iterable , expected in cases:
+            with self.subTest(iterable):
+                self.assertEqual(last(iterable),expected)
+                
+    def test_default(self):
+        for iterable, defualt, expected in [
+            (range(1),None, 0),
+            ([], None, None),
+            ({}, None, None),
+            (iter([]), None, None)
+        ]:
+            with self.subTest(args=(iterable,defualt)):
+                self.assertEqual(last(iterable,defualt),expected)
+                
+    def test_emptry(self):
+        for iterable in ([],iter(range(0))):
+            with self.subTest(iterable):
+                with self.assertRaises(ValueError):
+                    last(iterable)   
+            
