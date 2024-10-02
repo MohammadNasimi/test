d28e0465d3e95ece15ce25a3eaa1f40e0d51913a
MohammadNasimi <mohammadnasimi6803@gmail.com>
2024-10-02 12:24:46 +0330

    map_if

--- more_itertools_/more.py
+++ more_itertools_/more.py
@@ -170,4 +170,8 @@ def split_into(iterable,sizes):
             return
         else:
             yield list(islice(it, size))
-            
+    
+
+def map_if(iterable,pred,func,func_else=lambda x:x):
+    for item in iterable:
+        yield func(item) if pred(item) else func_else(item)
